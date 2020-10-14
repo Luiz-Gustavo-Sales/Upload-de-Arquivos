58408671256c505d3e677c67e1d0f172889438a1
Luiz Gustavo <luizgustavo151999@gmail.com>
2020-10-14 01:16:15 -0300

    UPLOAD DE ARQUIVOS CSV

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+//const csvtojson = require('csvtojson');
+//const csv = require('fast-csv');
+import Papa from 'papaparse';
 function App() {
+  const [Csv, setCsv] = useState(['']);
+
+  const handlechange = (event) => {
+    if (event.target.files[0] != '') {
+      setCsv({ file: event.target.files[0] });
+    }
+  };
+  const changeUpload = () => {
+    console.log('olha aqui ', Csv);
+    Papa.parse(
+      Csv,
+      {
+        worker: true,
+      },
+      function (resultingBlob) {
+        console.log('OLHA AQUIIIIII', resultingBlob);
+      }
+    );
+  };
   return (
     <div className="App">
+      <h2> Insira o arquivo para tratar os dados</h2>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          accept=".csv"
+          type="file"
+          name="file"
+          className="input-upload"
+          onChange={handlechange}
+        />
+        <button onClick={changeUpload}>Enviar</button>
       </header>
     </div>
   );
 }
-
 export default App;
